d456a1c47b15b965dd49d250eae39167c6a7e2ec
mayurpatel20 <patel.mayurki@northeastern.edu>
2024-09-19 20:09:27 -0400

    Assignment1

---
+++ Assignment1/src/model/Personprofile.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author mayurshiroya
+ */
+public class Personprofile {
+    String firstname;
+    String lastname;
+    String dateofbirth;
+    String nationality;
+    String gender;
+    String email;
+    String educationlevel;
+    String Phone;
+    String emergencycontactphone;
+    String passportnumber;
+    String driverslicensenumber;
+    String driverslicensestate;
+    String socialsecuritynumber;
+    String addressline1;
+    String addressline2;
+    String city;
+    String state;
+    String zip;
+    String telephonenumber;
+    String faxnumber;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEducationlevel() {
+        return educationlevel;
+    }
+
+    public void setEducationlevel(String educationlevel) {
+        this.educationlevel = educationlevel;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getEmergencycontactphone() {
+        return emergencycontactphone;
+    }
+
+    public void setEmergencycontactphone(String emergencycontactphone) {
+        this.emergencycontactphone = emergencycontactphone;
+    }
+
+    public String getPassportnumber() {
+        return passportnumber;
+    }
+
+    public void setPassportnumber(String passportnumber) {
+        this.passportnumber = passportnumber;
+    }
+
+    public String getDriverslicensenumber() {
+        return driverslicensenumber;
+    }
+
+    public void setDriverslicensenumber(String driverslicensenumber) {
+        this.driverslicensenumber = driverslicensenumber;
+    }
+
+    public String getDriverslicensestate() {
+        return driverslicensestate;
+    }
+
+    public void setDriverslicensestate(String driverslicensestate) {
+        this.driverslicensestate = driverslicensestate;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getAddressline1() {
+        return addressline1;
+    }
+
+    public void setAddressline1(String addressline1) {
+        this.addressline1 = addressline1;
+    }
+
+    public String getAddressline2() {
+        return addressline2;
+    }
+
+    public void setAddressline2(String addressline2) {
+        this.addressline2 = addressline2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephonenumber() {
+        return telephonenumber;
+    }
+
+    public void setTelephonenumber(String telephonenumber) {
+        this.telephonenumber = telephonenumber;
+    }
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+ 
+}
